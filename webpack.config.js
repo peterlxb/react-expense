8452add23cc85108550bd28f205102a6a73764c0
peterlxb <peterlxb@gmail.com>
2018-07-27 08:04:52 +0800

    Set up project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "public",
+    overlay: true,
+    hot: true,
+    stats: {
+      color: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
